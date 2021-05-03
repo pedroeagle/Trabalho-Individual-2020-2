dc830f6b8674d797c71977bc49cbe531a621e634
Pedro Igor <pedroigor.silva@outlook.com>
2021-05-03 13:04:58 -0300

    Print all env variables

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import 'dotenv/config';
 
 Vue.prototype.$axios = axios
-
+console.log(process.env);
 const api = axios.create({ baseURL: process.env.API_HOST.replace('"', '').replace('"', '')})
 Vue.prototype.$api = api
 
